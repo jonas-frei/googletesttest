1ee5f67b3baf98d40e9e2802486b775fbb1c6fb4
Jonas Frei <freij@protonmail.ch>
2022-06-16 20:19:18 +0000

    Link gtest_main and add more tests

--- mytests.cpp
+++ mytests.cpp
@@ -11,7 +11,16 @@ TEST(myfunction, add)
     ASSERT_NE(30, add(10, 22));
 }
 
-TEST(myfunction, addThrow)
+TEST(myfunction, divide)
+{
+    ASSERT_EQ(0, divide(0,10));
+    ASSERT_EQ(0, divide(5,10));
+    ASSERT_EQ(1, divide(10,10));
+    ASSERT_EQ(1, divide(15,10));
+    ASSERT_EQ(2, divide(20,10));
+}
+
+TEST(myfunction, divideThrow)
 {
     ASSERT_ANY_THROW(divide(10,0));
     ASSERT_NO_THROW(divide(0,10));
@@ -22,8 +31,8 @@ TEST(myfunction, subtract)
     ASSERT_EQ(-12, subtract(10, 22));
 }
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
+}*/
